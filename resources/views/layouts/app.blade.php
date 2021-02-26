775d1f9c5d5d5cbac9a99faf4c8abdceb731b55d
aryaseptiawan <aryaseptiawan18@gmail.com>
2021-02-27 01:17:26 +0700

    Pratikum3-task-2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,3 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>{{ config('app.name', 'PRATIKUM2') }}</title>
+<!-- 
+Cafe House Template
+http://www.templatemo.com/tm-466-cafe-house
+-->
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,600,700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Damion' rel='stylesheet' type='text/css'>
+  <link href={{ asset('css/bootstrap.min.css') }} rel="stylesheet">
+  <link href={{ asset('css/font-awesome.min.css') }} rel="stylesheet">
+  <link href={{ asset('css/templatemo-style.css') }} rel="stylesheet">
+  <link rel="shortcut icon" href={{ asset('img/favicon.ico') }} type="image/x-icon" />
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+  </head>
+  <body>
+    <!-- Preloader -->
+    <div id="loader-wrapper">
+      <div id="loader"></div>
+      <div class="loader-section section-left"></div>
+      <div class="loader-section section-right"></div>
+    </div>
+    <!-- End Preloader -->
+    <div class="tm-top-header">
+      <div class="container">
+        <div class="row">
+          <div class="tm-top-header-inner">
+            <div class="tm-logo-container">
+              <img src={{ asset('img/logo.png') }} alt="Logo" class="tm-site-logo">
+              <h1 class="tm-site-name tm-handwriting-font">Cafe House - Arya & Alvi</h1>
+            </div>
+            <div class="mobile-menu-icon">
+              <i class="fa fa-bars"></i>
+            </div>
+            <nav class="tm-nav">
+              <ul>
+                <li><a href="index.html" class="active">Home</a></li>
+                <li><a href="today-special.html">Today Special</a></li>
+                <li><a href="menu.html">Menu</a></li>
+                <li><a href="contact.html">Contact</a></li>
+              </ul>
+            </nav>   
+          </div>           
+        </div>    
+      </div>
+    </div>
+    <section class="tm-welcome-section">
+      <div class="container tm-position-relative">
+        <div class="tm-lights-container">
+          <img src={{ asset('img/light.png') }} alt="Light" class="light light-1">
+          <img src={{ asset('img/light.png') }} alt="Light" class="light light-2">
+          <img src={{ asset('img/light.png') }} alt="Light" class="light light-3">  
+        </div>        
+        <div class="row tm-welcome-content">
+          <h2 class="white-text tm-handwriting-font tm-welcome-header"><img src={{ asset('img/header-line.png') }} alt="Line" class="tm-header-line">&nbsp;Welcome To&nbsp;&nbsp;<img src={{ asset('img/header-line.png') }} alt="Line" class="tm-header-line"></h2>
+          <h2 class="gold-text tm-welcome-header-2">Cafe House</h2>
+          <p class="gray-text tm-welcome-description">Cafe House template is a mobile-friendly responsive <span class="gold-text">Bootstrap v3.3.5</span> layout by <span class="gold-text">templatemo</span>. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculusnec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
+          <a href="#main" class="tm-more-button tm-more-button-welcome">Details</a>      
+        </div>
+        <img src={{ asset('img/table-set.png')}} alt="Table Set" class="tm-table-set img-responsive">             
+      </div>      
+    </section> 
+    <footer>
+      <div class="tm-black-bg">
+        <div class="container">
+          <div class="row margin-bottom-60">
+            <nav class="col-lg-3 col-md-3 tm-footer-nav tm-footer-div">
+              <h3 class="tm-footer-div-title">Main Menu</h3>
+              <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Directory</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Our Services</a></li>
+              </ul>
+            </nav>
+            <div class="col-lg-5 col-md-5 tm-footer-div">
+              <h3 class="tm-footer-div-title">About Us</h3>
+              <p class="margin-top-15">Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.</p>
+              <p class="margin-top-15">Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus.</p>
+            </div>
+            <div class="col-lg-4 col-md-4 tm-footer-div">
+              <h3 class="tm-footer-div-title">Get Social</h3>
+              <p>Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante.</p>
+              <div class="tm-social-icons-container">
+                <a href="#" class="tm-social-icon"><i class="fa fa-facebook"></i></a>
+                <a href="#" class="tm-social-icon"><i class="fa fa-twitter"></i></a>
+                <a href="#" class="tm-social-icon"><i class="fa fa-linkedin"></i></a>
+                <a href="#" class="tm-social-icon"><i class="fa fa-youtube"></i></a>
+                <a href="#" class="tm-social-icon"><i class="fa fa-behance"></i></a>
+              </div>
+            </div>
+          </div>          
+        </div>  
+      </div>      
+      <div>
+        <div class="container">
+          <div class="row tm-copyright">
+           <p class="col-lg-12 small copyright-text text-center">Copyright &copy; 2084 Your Cafe House Arya & Alvi</p>
+         </div>  
+       </div>
+     </div>
+   </footer> <!-- Footer content-->  
+   <!-- JS -->
+   <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>      <!-- jQuery -->
+   <script type="text/javascript" src="js/templatemo-script.js"></script>      <!-- Templatemo Script -->
+
+ </body>
+ </html>
+
+
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
@@ -7,7 +129,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'PRATIKUM2') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -24,7 +146,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'PRATIKUM2') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>

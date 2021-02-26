0db1e45e61d3932b33b9c2432a1cc0ba53bc08d7
aryaseptiawan <aryaseptiawan18@gmail.com>
2021-02-26 20:12:54 +0700

    Praktikum 1 View Web

---
+++ resources/views/contact-us.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact Us</title>
+</head>
+<body>
+    <h1>Ini Halaman Contact-us</h1>
+    <h2>Arya Septiawan</h2>
+    <h2>Teknik Informatika</h2>
+</body>
+</html>
